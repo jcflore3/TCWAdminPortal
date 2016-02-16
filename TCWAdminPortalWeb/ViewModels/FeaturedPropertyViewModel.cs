e96035005762c06d82b7593b46b7a74dae2b8c28
jcflore3 <jc.flores556@gmail.com>
2016-02-16 17:45:25 -0600

    Check in for the night

--- TCWAdminPortalWeb/ViewModels/FeaturedPropertyViewModel.cs
+++ TCWAdminPortalWeb/ViewModels/FeaturedPropertyViewModel.cs
@@ -6,29 +6,51 @@ namespace TCWAdminPortalWeb.ViewModels
     public class FeaturedPropertyViewModel
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255, MinimumLength = 5)]
         public string Title { get; set; }
+
+        [Display(Name = "Image URL")]
         public string ImageURL { get; set; }
+
         [Required]
         [StringLength(255, MinimumLength = 5)]
+        [Display(Name = "Street Address")]
         public string StreetAddress { get; set; }
+
         [Required]
         [StringLength(255, MinimumLength = 5)]
         public string City { get; set; }
+
         [Required]
         [StringLength(10, MinimumLength = 2)]
         public string State { get; set; }
+
         [Required]
         [StringLength(5, MinimumLength = 5)]
         public string Zip { get; set; }
+
+        [Display(Name = "Bed Rooms")]
         public int BedRooms { get; set; }
+
+        [Display(Name = "Full Baths")]
         public int FullBaths { get; set; }
+
+        [Display(Name = "Half Baths")]
         public int HalfBaths { get; set; }
+
         public int Garage { get; set; }
+
+        [Display(Name = "Building Sq Feet")]
         public int BuildingSQFeet { get; set; }
+
+        [Display(Name = "Lot Sq Feet")]
+
         public int LotSQFeet { get; set; }
+
         public DateTime Created { get; set; } = DateTime.UtcNow;
+
         public Boolean Enabled { get; set; }
     }
 }
